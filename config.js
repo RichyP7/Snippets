06589c336cdc56f09521bbd8568e4619f8a012aa
richy <richardputz@gmx.at>
2018-05-24 18:34:46 +0200

    add

---
+++ config.js
@@ -0,0 +1,6 @@
+let config = {};
+
+config.storageAccount = "richardstorageacc"
+config.storageKey = "UgD7zPG1M9NOjkeSAsB9YwowjwvR/+C/1N/7X8Z7dldBD3YvIaynlH1g4+zrQqnEj+bbtfGm4P+g3U8JwvwW+A=="
+
+module.exports = config;
